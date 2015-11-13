abbee1b2bb24928b8ec39037772fb2cfddaf1e79
Hhardi <vigurk@gmail.com>
2015-11-13 13:08:53 +0200

    2. Tere maailm button [Delivers #108068846]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -2,9 +2,16 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <title>Document</title>
 </head>
 <body>
+<button onclick="myFunction()">Tere maailm</button>
+<script>
+    function myFunction() {
+        alert("Tere maailm");
+    }
+</script>
 
 </body>
 </html>
