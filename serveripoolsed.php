2387f7d2ccf0c1b083df6f25adbf934d787b83ef
Hhardi <vigurk@gmail.com>
2015-11-13 14:48:07 +0200

    16. Massiivid [Delivers #103878064]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -4,11 +4,13 @@ $enimi =" Hardi";
 $pnimi = " Kaiv ";
 $vanus =  19;
 $eesnimi_algab_vokaaliga = false;
+$isik = array($enimi, $pnimi,'('. $vanus.')');
 
 echo $enimi;
 echo $pnimi;
 echo '('.$vanus.')';
 echo '<br>';
+echo $isik[0].$isik[1].$isik[2];
 
 
 
