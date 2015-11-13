81db729af522ef11418442d2dff188bae05f0ce2
Hhardi <vigurk@gmail.com>
2015-11-13 15:00:57 +0200

    20. Floats [Delivers #103878072]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -5,6 +5,9 @@ $pnimi = " Kaiv ";
 $vanus =  19;
 $eesnimi_algab_vokaaliga = false;
 $isik = array($enimi, $pnimi,'('. $vanus.')');
+$var1 = 1.23;
+$var2= 2.34;
+
 
 echo $enimi;
 echo $pnimi;
@@ -15,6 +18,7 @@ echo $isik[0].$isik[1].$isik[2];
 if ($eesnimi_algab_vokaaliga) {
     echo 'Nimi algab vokaaliga';
 }
+echo '<br>'.($var1 + $var2);
 
 
 ?>
