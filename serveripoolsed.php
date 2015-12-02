6390249fcff8249c7d9263ac976b8cdbace0f5c4
Hhardi <vigurk@gmail.com>
2015-12-02 13:25:21 +0200

    19. funktsioonid

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -1,21 +1,21 @@
 <?php
-$enimi =" Hardi";
+$enimi ="Hardi";
 $pnimi = " Kaiv ";
 $vanus =  19;
 $eesnimi_algab_vokaaliga = false;
 $isik = array($enimi, $pnimi,'('. $vanus.')');
 $var1 = 1.23;
 $var2= 2.34;
-
-
-
-
 if ($eesnimi_algab_vokaaliga) {
     echo 'Nimi algab vokaaliga';
 }
 
 $obj=(object)array('nimi'=>'hardi', 'pnimi'=> 'kaiv', 'vanus'=>19);
-
 echo $obj->nimi. " " .$obj->pnimi. " (".$obj->vanus.")" ;
-
+$vokaalid = array('A', 'E', 'I', 'O', 'U','Õ', 'Ä', 'Ö' );
+echo '<br>';
+if (in_array($enimi[0], $vokaalid)) {
+    echo "Eesnimi algab vokaaliga";
+}
+else {echo 'Eesnimi ei alga vokaaliga';}
 ?>
